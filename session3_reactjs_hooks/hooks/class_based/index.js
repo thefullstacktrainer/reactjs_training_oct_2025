17094746581c8d7fc6d52c7a26d0e7a51235a028
Lakshmikant <fullstacktrainer4u@gmail.com>
2025-10-19 20:56:36 +0530

    class

--- session3_reactjs_hooks/hooks/class_based/index.js
+++ session3_reactjs_hooks/hooks/class_based/index.js
@@ -1,9 +1,34 @@
-class AccountInformation extends React.Component{
-    constructor(){
-
+class AccountInformation extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = { balance: 100000 }
+        this.handleClick = this.handleClick.bind(this);
     }
 
-    render(){
-        
+    handleClick(){
+
+        console.log("Test called" +this.state.balance)
+    }
+
+    componentDidMount() {
+        this.handleClick()
+        console.log("componentDidMount called")
+    }
+    componentDidUpdate() {
+        console.log("componentDidUpdate called")
+    }
+    componentWillUnmount() {
+        console.log("componentWillUnmount called")
+    }
+
+    render() {
+        return <div>
+            <h3>Account Balance : Rupees {this.state.balance}</h3>
+            <button onClick={this.handleClick}> Click</button>
+        </div>
     }
 }
+
+const root = ReactDOM.createRoot(document.getElementById("app"));
+root.render(<AccountInformation />)
+
