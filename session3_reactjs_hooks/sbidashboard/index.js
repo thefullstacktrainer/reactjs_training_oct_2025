949f775cd4b31398697cef17c79e91e6fb4fae32
Lakshmikant <fullstacktrainer4u@gmail.com>
2025-10-19 22:43:03 +0530

    Added Credit Card Account

--- session3_reactjs_hooks/sbidashboard/index.js
+++ session3_reactjs_hooks/sbidashboard/index.js
@@ -1,6 +1,6 @@
 const { useState } = React;
 
-function SBIDashboard({ name: customerName, accountNumber, branch, hasDeposit, hasLoan }) {
+function SBIDashboard({ name: customerName, accountNumber, branch, hasDeposit, hasLoan, hasCreditCard }) {
     console.log(hasDeposit)
     return (<div style={{ padding: "20px", fontFamily: "Arial" }}>
         <h2>SBI Banking Dashboard</h2>
@@ -11,6 +11,7 @@ function SBIDashboard({ name: customerName, accountNumber, branch, hasDeposit, h
 
         {hasDeposit && <DepositAccount />}
         {hasLoan && <LoanAccount />}
+         {hasCreditCard && <CreditCardAccount />}
     </div>);
 }
 
@@ -40,7 +41,24 @@ function DepositAccount() {
     </div>)
 }
 
+function CreditCardAccount(){
+    const [creditLimit, setCreditLimit] = useState(1000000);
+    const [spent, setSpent] = useState(200000);
+    const [available, setAvailable] = useState(creditLimit - spent)
+    function updateCreditAmount(amount){
+        setSpent(spent + amount)
+        setAvailable(available - amount)
+    }
+return (<div style={{ border: "1px solid #0077b6", padding: "10px", margin: "10px" }}>
+        <h3>Credit Card</h3>
+        <p>Credit Limit : Rs:{creditLimit}</p>
+        <p>Spent : {spent}</p>
+        <p>Available : {available}</p>
+        <button onClick={() => updateCreditAmount(10000)}>Spend 10000</button>
+    </div>)
+}
+
 
 const root = ReactDOM.createRoot(document.getElementById("app"));
 root.render(<SBIDashboard name="Harish" accountNumber="46858" branch="Andheri East"
-    hasDeposit={true} hasLoan={true} />)
+    hasDeposit={true} hasLoan={true} hasCreditCard={true}/>)
