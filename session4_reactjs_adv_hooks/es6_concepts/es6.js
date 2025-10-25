d8691bd9d2d5841e6a8dbcbe6dcd978a9af59ba7
Lakshmikant <fullstacktrainer4u@gmail.com>
2025-10-25 22:11:21 +0530

    import and export

---
+++ session4_reactjs_adv_hooks/es6_concepts/es6.js
@@ -0,0 +1,14 @@
+// import & export
+
+import log from "./logger.js"
+
+log("Hello World")
+
+
+import { add, sub, multiply } from "./math.js";
+
+log(add(3,4))
+
+log(sub(13,4))
+
+log(multiply(3,4))
