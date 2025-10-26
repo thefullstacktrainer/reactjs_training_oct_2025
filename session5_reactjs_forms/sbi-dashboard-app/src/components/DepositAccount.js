f713a63f57da9ea640587f1d80a474b012d6272d
Lakshmikant <fullstacktrainer4u@gmail.com>
2025-10-26 21:17:30 +0530

    Application build with create react app

---
+++ session5_reactjs_forms/sbi-dashboard-app/src/components/DepositAccount.js
@@ -0,0 +1,21 @@
+import { useState } from 'react';
+import helper from "../utils/helper";
+
+function DepositAccount() {
+  const [balance, setBalance] = useState(100000);
+
+  return (
+    <div className="account-card">
+      <h3>🏦 Deposit Account</h3>
+      <p>Balance: {helper.formatCurrency(balance)}</p>
+      <p>Interest: (3.5%)</p>
+
+      <div className="actions">
+        <button onClick={() => setBalance(balance + 1000)}>Deposit ₹1000</button>
+        <button onClick={() => setBalance(balance - 1000)}>Withdraw ₹1000</button>
+      </div>
+    </div>
+  );
+}
+
+export default DepositAccount;
