c661ab9b9c6c9ca8c7f66b6b0fd697458f35315e
Lakshmikant <fullstacktrainer4u@gmail.com>
2025-11-01 21:22:51 +0530

    Nav link added

--- session6_reactjs_navigation/employee-management/src/App.jsx
+++ session6_reactjs_navigation/employee-management/src/App.jsx
@@ -1,4 +1,4 @@
-import { Link, Routes, Route } from "react-router-dom"
+import { Link, Routes, Route, NavLink } from "react-router-dom"
 import Home from "./pages/Home"
 import About from "./pages/About"
 import Contact from "./pages/Contact"
@@ -9,9 +9,21 @@ function App() {
       <div style={{ padding: '2rem' }}>
         <h1>Employee Management Application</h1>
         <nav style={{ marginBottom: '1rem' }}>
-          <Link to="/" style={{ marginBottom: '1rem' }}>Home</Link>
-          <Link to="/about" style={{ marginBottom: '1rem' }}>About</Link>
-          <Link to="/contact" style={{ marginBottom: '1rem' }}>Contact</Link>
+          <NavLink to="/" style={({isActive}) => ({
+            marginRight: '1rem',
+            textDecoration: isActive ? 'underline' : 'none',
+            color: isActive ? 'blue' : 'black'
+          })}>Home</NavLink>
+          <NavLink to="/about" style={({isActive}) => ({
+            marginRight: '1rem',
+            textDecoration: isActive ? 'underline' : 'none',
+            color: isActive ? 'blue' : 'black'
+          })}>About</NavLink>
+          <NavLink to="/contact" style={({isActive}) => ({
+            marginRight: '1rem',
+            textDecoration: isActive ? 'underline' : 'none',
+            color: isActive ? 'blue' : 'black'
+          })}>Contact</NavLink>
         </nav>
         <Routes>
           <Route path="/" element={<Home />} />
