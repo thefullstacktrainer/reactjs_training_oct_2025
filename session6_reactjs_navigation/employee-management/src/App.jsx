066ef8932428e5c5ac3d6ed832efdee7dec938c9
Lakshmikant <fullstacktrainer4u@gmail.com>
2025-11-01 21:09:50 +0530

    added path

--- session6_reactjs_navigation/employee-management/src/App.jsx
+++ session6_reactjs_navigation/employee-management/src/App.jsx
@@ -1,13 +1,16 @@
+import { Link } from "react-router-dom"
+
 function App() {
 
   return (
     <>
-      <div style={{padding:'2rem'}}>
-          <h1>Employee Management Application</h1>
-          <nav style={{marginBottom:'1rem'}}>
-            
-
-          </nav>
+      <div style={{ padding: '2rem' }}>
+        <h1>Employee Management Application</h1>
+        <nav style={{ marginBottom: '1rem' }}>
+          <Link to="/" style={{ marginBottom: '1rem' }}>Home</Link>
+          <Link to="/about" style={{ marginBottom: '1rem' }}>About</Link>
+          <Link to="/contact" style={{ marginBottom: '1rem' }}>Contact</Link>
+        </nav>
       </div>
     </>
   )
