7e58d2e25ebb5deae71a4c2ec8924e7154d06d9e
Lakshmikant <fullstacktrainer4u@gmail.com>
2025-11-01 22:37:47 +0530

    added employee list

--- session6_reactjs_navigation/employee-management/src/pages/EmployeeList.jsx
+++ session6_reactjs_navigation/employee-management/src/pages/EmployeeList.jsx
@@ -1,7 +1,35 @@
 import React from 'react'
+import { useContext } from 'react'
+import { EmployeeContext } from '../context/EmployeeContext'
 
 export default function EmployeeList() {
-  return (
-    <div>EmployeeList</div>
-  )
+    const { employees } = useContext(EmployeeContext)
+    return (<>
+        <h2>Employee List</h2>
+        <table>
+            <thead>
+                <tr>
+                    <th>
+                        ID
+                    </th>
+                    <th>Name</th>
+                    <th>Role</th>
+                    <th>Department</th>
+                </tr>
+            </thead>
+            <tbody>
+                {employees.map((emp) => (
+                    <tr key={emp.id}>
+                    <td>{emp.id}</td>
+                    <td>{emp.name}</td>
+                    <td>{emp.role}</td>
+                    <td>{emp.department}</td>
+                </tr>
+
+                ))}
+            </tbody>
+        </table>
+    </>
+
+    )
 }
