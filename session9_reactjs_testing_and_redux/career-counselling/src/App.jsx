cf58817a8a395caaaf25a4a237db5b8928cd7168
Lakshmikant <fullstacktrainer4u@gmail.com>
2025-11-09 21:34:28 +0530

    basic unit test with ReactJS

--- session9_reactjs_testing_and_redux/career-counselling/src/App.jsx
+++ session9_reactjs_testing_and_redux/career-counselling/src/App.jsx
@@ -8,7 +8,9 @@ function App() {
       <div>
         <p>Welcome to last session of ReactJS</p>
 
-        <CareerCard title="SE" description="Software Engineer"/>
+        {/* <CareerCard title="SE" description="Software Engineer"/>
+        <CareerCard title="AI Engineer" description="Build AI models"/> */}
+        <CareerCard title="AI Engineer" description="Build AI models"/>
       </div>
     </>
   )
--- session9_reactjs_testing_and_redux/career-counselling/tests/CareerCard.test.jsx
+++ session9_reactjs_testing_and_redux/career-counselling/tests/CareerCard.test.jsx
@@ -23,4 +23,21 @@ describe("Career component unit test", () => {
         expect(screen.getByText("AI Engineer")).toBeInTheDocument()
     })
 
+
+     test("render the given description", () => {
+        render(<CareerCard title="AI Engineer" description="Build AI models"/>)
+        expect(screen.getByText("Build AI models")).toBeInTheDocument()
+    })
+
+    test("render heading inside h3 tag", () => {
+        render(<CareerCard title="Cloud Architect" description="Design Scalable Systems"/>)
+        const heading = screen.getByRole("heading", {level:3})
+        expect(heading).toHaveTextContent("Cloud Architect")
+    })
+
+       test("match the sanpshot for consistency", () => {
+        const {asFragment} = render(<CareerCard title="AI Engineer" description="Build AI models"/>)
+        expect(asFragment()).toMatchSnapshot()
+    })
+
 })
