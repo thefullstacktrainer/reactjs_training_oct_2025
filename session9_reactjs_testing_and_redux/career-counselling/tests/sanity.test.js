34ff9e930dcb9cd157ebb22f524fb26ef1bf9ce9
Lakshmikant <fullstacktrainer4u@gmail.com>
2025-11-09 21:08:51 +0530

    Sanity test

--- session9_reactjs_testing_and_redux/career-counselling/tests/sanity.test.js
+++ session9_reactjs_testing_and_redux/career-counselling/tests/sanity.test.js
@@ -17,4 +17,28 @@ describe("Sanity Tests", () => {
     test("should verify true as true", () => {
         expect(true).toBe(true)
     })
+
+    // test("should check basic arithmetic", () => {
+    //     expect(2 + 2).toBe(5)
+    // })
+    test("should check basic arithmetic", () => {
+        expect(2 + 2).toBe(4)
+    })
+
+    test("string concatenation", () => {
+        const greeting = "Career" + " Counselling";
+        expect(greeting).toBe("Career Counselling")
+    })
+    test("should verify an object structure", () => {
+        const career = { id: 1, title: "AI Engineer" }
+        expect(career).toHaveProperty("title", "AI Engineer")
+    })
+
+    test("should test a mocked function using vi.fn() ", () => {
+        const mockFn = vi.fn().mockReturnValue("Career Added")
+        const result = mockFn("AI Engineer")
+        expect(mockFn).toHaveBeenCalledWith("AI Engineer")
+        expect(result).toBe("Career Added")
+
+    })
 })
